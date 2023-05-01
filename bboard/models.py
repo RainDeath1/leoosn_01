b04a683ddfd1b6518bf968d3d16d36b53e7416a6
Голенков Давид <david53947@mail.ru>
2023-05-02 01:17:39 +0600

    Работа с шаблонами

--- bboard/models.py
+++ bboard/models.py
@@ -21,7 +21,32 @@ class Bb(models.Model):
         verbose_name='Опубликовано',
     )
 
+    rubric = models.ForeignKey(
+        'Rubric',
+        null=True,
+        on_delete=models.PROTECT,
+        verbose_name='Рубрика',
+    )
+
     class Meta:
         verbose_name_plural = "Объявления"
         verbose_name = "Объявление"
         ordering = ['-published']
+
+
+class Rubric(models.Model):
+    name = models.CharField(
+        max_length=20,
+        db_index=True,
+        verbose_name = 'Название'
+    )
+
+    class Meta:
+        verbose_name_plural = 'Рубрики'
+        verbose_name = 'Рубрика'
+        ordering = ['name']
+
+    def __str__(self):
+        return self.name
+
+
